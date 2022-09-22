e02b3393fcfc07f337a1d9dd69710356235d81bc
rromikas <rromikas@gmail.com>
2022-09-22 08:33:28 +0300

    build fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,17 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppContext as NextAppContext, AppProps } from "next/app";
 import { createContext, useState } from "react";
 import { CartItem } from "types";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import App from "next/app";
 
-interface AppCotnextType {
+interface AppContextType {
   cart: CartItem[];
   setCart: React.Dispatch<React.SetStateAction<CartItem[]>>;
 }
 
-export const AppContext = createContext<AppCotnextType>({} as any);
+export const AppContext = createContext<AppContextType>({} as any);
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [cart, setCart] = useState<CartItem[]>([]);
@@ -24,3 +25,8 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
+
+MyApp.getInitialProps = async (ctx: NextAppContext) => {
+  const appProps = await App.getInitialProps(ctx);
+  return { ...appProps };
+};
