aaa765cbb1fec30dde26e0423f90007ecc91e01b
rromikas <rromikas@gmail.com>
2022-09-22 07:50:30 +0300

    build fix

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,3 @@
-export const API_BASE_URL = process.env.NODE_ENV === "production" ? process.env.VERCEL_URL : "http://localhost:3000";
+// export const API_BASE_URL = process.env.NODE_ENV === "production" ? process.env.VERCEL_URL : "http://localhost:3000";
+
+export const API_BASE_URL = undefined;
